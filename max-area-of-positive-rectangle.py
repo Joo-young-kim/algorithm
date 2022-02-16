9c94e1c0d1256cd5ff42d8f20ddac2f0dd55666a
김주영 <89004835+Joo-young-kim@users.noreply.github.com>
2022-02-16 20:18:32 +0900

    Create max-area-of-positive-rectangle.py

---
+++ max-area-of-positive-rectangle.py
@@ -0,0 +1,18 @@
+n,m = tuple(map(int,input().split()))
+grid = [list(map(int,input().split())) for _ in range(n)]
+
+ans = -1
+def positive_rect(x1,y1,x2,y2):
+    for i in range(x1,x2+1):
+        for j in range(y1,y2+1):
+            if grid[i][j]<=0:
+                return False
+    return True
+
+for i in range(n):
+    for j in range(m):
+        for k in range(i,n):
+            for l in range(j,m):
+                if positive_rect(i,j,k,l):
+                    ans = max(ans,(k-i+1)*(l-j+1))
+print(ans)
